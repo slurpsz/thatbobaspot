5d944314f9e1868081de8d2f55bf7fc2327fb7b1
slurpeesz <khtbee@gmail.com>
2023-08-19 19:49:57 -0700

    created hamburger menu bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import image from "../assets/B.png"
+import { FaBars} from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav)
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <div className="flex justify-between pr-4">
+        <img src={image} alt="logo" style={{ width: 200 }} />
+        <ul className="items-center uppercase hidden md:flex">
+          <li className="font-bold p-6">Home</li>
+          <li className="font-bold p-6">Menu</li>
+          <li className="font-bold p-6">About</li>
+          <li className="font-bold p-6">Locations</li>
+          <li className="font-bold p-6">Careers</li>
+          <li className="font-bold p-6">Contact</li>
+        </ul>
+
+        <div onClick={handleClick} className="flex pt-8 md:hidden">
+          {!nav ? <FaBars /> : <AiOutlineClose />}
+        </div>
+
+        {/* Mobile Menu */}
+        <div
+          className={
+            !nav
+              ? "hidden"
+              : "fixed right-8 top-8 h-full border-l border-l-grass"
+          }
+        >
+          <ul className={!nav ? "hidden" : "p-4 uppercase cursor-pointer"}>
+            <li className="font-bold p-6 hover:text-sage">Home</li>
+            <li className="font-bold p-6">Menu</li>
+            <li className="font-bold p-6">About</li>
+            <li className="font-bold p-6">Locations</li>
+            <li className="font-bold p-6">Careers</li>
+            <li className="font-bold p-6">Contact</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "sage": "#6b7b62",
+      "grass": "#d4d4ce", 
+    }  
   },
   plugins: [],
 };
